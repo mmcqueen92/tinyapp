fdcb4ecca07d0d8e01b33f53ff4ff79a0a31e5eb
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-06 05:27:17 +0000

    created and wrote core code for urls_new.ejs in views dir. updated express_server.js with app.post to /urls path

--- express_server.js
+++ express_server.js
@@ -8,6 +8,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+generateRandomString = (stringLength) => {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < stringLength; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,17 +32,27 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = urlDatabase;
   res.render("urls_index", {
     templateVars: templateVars
   });
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
