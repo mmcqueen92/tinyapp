7135dd5488d3f86699cd81d78edf81bb90af1faf
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-08 22:28:21 +0000

    installed bcryptjs. updated express_server.js to encrypt passwords in database and check passwords on login using bcrypt.compareSync

--- express_server.js
+++ express_server.js
@@ -1,15 +1,20 @@
+// --- REQUIREMENTS ---
 const express = require("express");
 const app = express();
 const PORT = 8080;
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 
+
+/// --- MIDDLEWARE ---
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 
+/// --- DATABASES --- (SHOULD THESE BE ARRAYS?)
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: 'default' },
   "9sm5xK": { longURL: "http://www.google.com", userID: 'default' }
@@ -28,8 +33,10 @@ const users = {
   },
 };
 
+
+/// --- FUNCTIONS --- (MODIFY TO IMPORT?)
 const userExists = (givenEmail) => {
-  for (user in users) {
+  for (let user in users) {
     if (users[user].email === givenEmail) {
       return true;
     }
@@ -38,7 +45,7 @@ const userExists = (givenEmail) => {
 };
 
 const findUser = (givenEmail) => {
-  for (user in users) {
+  for (let user in users) {
     if (users[user].email === givenEmail) {
       return user;
     }
@@ -46,47 +53,43 @@ const findUser = (givenEmail) => {
 };
 
 const generateRandomString = (stringLength) => {
-  return (Math.random().toString(36).slice(2, stringLength + 2))
-}
+  return (Math.random().toString(36).slice(2, stringLength + 2));
+};
 
+
+// --- BREAD ---
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// --- JSON URLDATABASE ---
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// --- HELLO ---
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// --- GET MAIN PAGE ---
 app.get("/urls", (req, res) => {
   const user = users[req.cookies.user_id];
   const urls = {};
-  for (url in urlDatabase) {
+  for (let url in urlDatabase) {
     if (urlDatabase[url].userID === req.cookies.user_id) {
       urls[url] = urlDatabase[url];
     }
   }
   const templateVars = { urls: urls, users: users, user: user };
   res.render("urls_index", templateVars);
-})
-
-app.get("/urls/new", (req, res) => {
-  const user = users[req.cookies.user_id];
-  if (!user) {
-    res.redirect("/login");
-  } else if (user) {
-    const templateVars = { user: user }
-    res.render("urls_new", templateVars);
-  }
 });
 
+// --- POST NEW URL ---
 app.post("/urls", (req, res) => {
   const user = users[req.cookies.user_id];
   if (!user) {
-    res.send("You cannot create new shortened URLs because you are not logged in.")
+    res.send("You cannot create new shortened URLs because you are not logged in.");
   } else if (user) {
     const id = generateRandomString(6);
     urlDatabase[id] = { longURL: req.body.longURL, userID: req.cookies.user_id, };
@@ -94,102 +97,122 @@ app.post("/urls", (req, res) => {
   }
 });
 
+// --- GET CREATE NEW PAGE ---
+app.get("/urls/new", (req, res) => {
+  const user = users[req.cookies.user_id];
+  if (!user) {
+    res.redirect("/login");
+  } else if (user) {
+    const templateVars = { user: user };
+    res.render("urls_new", templateVars);
+  }
+});
 
+// --- GET REGISTER PAGE ---
 app.get("/register", (req, res) => {
   const user = users[req.cookies.user_id];
   if (user) {
     res.redirect("/urls");
   }
-  const templateVars = { user: user }
+  const templateVars = { user: user };
   res.render("register", templateVars);
-})
-
-app.post("/urls/:id", (req, res) => {
-  const user = users[req.cookies.user_id].id;
-  console.log('user: ', user)
-  if (!user || user !== urlDatabase[req.params.id].userID) {
-    res.send("Error 403: You don't have permission to do that.")
-  } else if (!urlDatabase[req.params.id]) {
-    res.send("Given URL doesnt exist and cannot be modified.")
-  } else if (user === urlDatabase[req.params.id].userID) {
-    const id = req.params.id;
-    const newURL = req.body.newURL;
-    urlDatabase[id].longURL = newURL;
-    const templateVars = { user: user }
-    res.redirect(`/urls`)
-  }
-})
-
-app.post("/urls/:id/delete", (req, res) => {
-  if (!req.cookies.user_id) {
-    res.send("Error 403: You don't have permission to do that.")
-  } else if (req.cookies.user_id) {
-    const user = users[req.cookies.user_id].id;
-    if (!urlDatabase[req.params.id]) {
-      res.send("Error 404 page not found");
-    } else if (!user || user !== urlDatabase[req.params.id].userID) {
-      res.send("Error 403: You don't have permission to do that.")
-    } else if (!urlDatabase[req.params.id]) {
-      res.send("Given URL doesnt exist and cannot be modified.")
-    } else if (user === urlDatabase[req.params.id].userID) {
-      delete urlDatabase[req.params.id];
-      res.redirect("/urls")
-    }
-  }
 });
 
-app.post("/login", (req, res) => {
-  if (!userExists(req.body.email)) {
-    res.send('Error 403: Email or Password is incorrect.');
-  } else if (userExists(req.body.email)) {
-    const user = findUser(req.body.email);
-    if (users[user].password !== req.body.password) {
-      res.send('Error 403: Email or Password is incorrect.');
-    } else if (users[user].password === req.body.password) {
-      res.cookie("user_id", user)
-      res.redirect("/urls");
-    }
-  }
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect("/urls");
-})
-
+// --- POST REGISTER ---
 app.post("/register", (req, res) => {
-  if (req.body.email.length < 5 || req.body.password.length < 1) {
+  if (!req.body.email || !req.body.password) {
     res.send("Error 400: Invalid Request.\nPlease enter a valid email address and a password.");
-  } else if (userExists(req.body.email) === true) {
+  } else if (userExists(req.body.email)) {
     res.send('Email is already registered');
   } else {
     const newUserId = generateRandomString(6);
-    users[newUserId] = { id: newUserId, email: req.body.email, password: req.body.password };
+    const password = req.body.password;
+    const hashedPass = bcrypt.hashSync(password, 10);
+    users[newUserId] = { id: newUserId, email: req.body.email, password: hashedPass };
     res.cookie("user_id", newUserId);
     res.redirect("/urls");
   }
-})
+});
 
+// --- GET LOGIN PAGE ---
 app.get("/login", (req, res) => {
   const user = users[req.cookies.user_id];
   if (user) {
     res.redirect("/urls");
   }
-  const templateVars = { user: user }
+  const templateVars = { user: user };
   res.render("login", templateVars);
-})
+});
 
+// --- POST LOGIN ---
+app.post("/login", (req, res) => {
+  if (!userExists(req.body.email)) {
+    res.send('Error 403: Email or Password is incorrect.');
+  } else if (userExists(req.body.email)) {
+    const user = findUser(req.body.email);
+    const userHashedPass = users[user].password;
+    const givenPassword = req.body.password;
+    if (!bcrypt.compareSync(givenPassword, userHashedPass)) {
+      res.send('Error 403: Email or Password is incorrect.');
+    } else if (bcrypt.compareSync(givenPassword, userHashedPass)) {
+      res.cookie("user_id", user);
+      res.redirect("/urls");
+    }
+  }
+});
+
+// --- POST LOGOUT ---
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect("/urls");
+});
+
+
+
+// --- GET VIEW SHORTENED URL PAGE ---
 app.get("/urls/:id", (req, res) => {
   const user = users[req.cookies.user_id];
   if (urlDatabase[req.params.id]) {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: user };
-    const urlDatabaseKey = templateVars.id;
     res.render("urls_show", templateVars);
-  } else if (!urlDatabase.urlDatabaseKey) {
-    res.send("Error 404 page not found");
+  } else if (!urlDatabase[req.params.id]) {
+    res.status(404).send("Page not found");
   }
 });
 
+// --- POST DELETE URL ---
+app.post("/urls/:id/delete", (req, res) => {
+  if (!req.cookies.user_id) {
+    res.status(403).send("Error 403: You don't have permission to do that.");
+  } else if (req.cookies.user_id) {
+    const user = users[req.cookies.user_id].id;
+    if (!urlDatabase[req.params.id]) {
+      res.status(404).send("Error 404 page not found");
+    } else if (!user || user !== urlDatabase[req.params.id].userID) {
+      res.status(403).send("Error 403: You don't have permission to do that.");
+    } else if (user === urlDatabase[req.params.id].userID) {
+      delete urlDatabase[req.params.id];
+      res.redirect("/urls");
+    }
+  }
+});
+
+// --- POST EDIT URL ---
+app.post("/urls/:id", (req, res) => {
+  const user = users[req.cookies.user_id].id;
+  if (!user || user !== urlDatabase[req.params.id].userID) {
+    res.status(403).send("Error 403: You don't have permission to do that.");
+  } else if (!urlDatabase[req.params.id]) {
+    res.send("Given URL doesnt exist and cannot be modified.");
+  } else if (user === urlDatabase[req.params.id].userID) {
+    const id = req.params.id;
+    const newURL = req.body.newURL;
+    urlDatabase[id].longURL = newURL;
+    res.redirect(`/urls`);
+  }
+});
+
+// --- LISTEN ON PORT X ---
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
